ebdfae10d72867674a14ce226dc0c7dbfcf8b826
Frolov Sergey <bigcheeseh@gmail.com>
2019-03-12 18:07:36 +0200

    feat(initStructure): ✨  [#163386008] init structure

--- src/Containers/RootContainer/index.tsx
+++ src/Containers/RootContainer/index.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View, Text } from "react-native";
 import ButtonsContainer from "../ButtonsContainer";
+import Colors from "../../Themes/Colors";
 
 class Root extends React.PureComponent<{}> {
   public render() {
     return (
       <View style={styles.container}>
-        <View style={styles.leftPanel} />
+        <View style={styles.leftPanel}>
+          <Text style={styles.label}>Buttons</Text>
+        </View>
         <ButtonsContainer />
       </View>
     );
@@ -25,6 +28,12 @@ const styles = StyleSheet.create({
     width: "15%",
     elevation: 4,
     backgroundColor: "#2979FF",
+    alignItems: "center",
+  },
+  label: {
+    color: Colors.buttonText,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
 
