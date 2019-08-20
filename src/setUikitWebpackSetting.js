e0467184432a96eb7ad757ee6f211fe1cb5b3f78
Frolov Sergey <bigcheeseh@gmail.com>
2019-08-20 20:32:07 +0300

    fix(form init values): 🐛  change login form init values [#167995796]

--- src/setUikitWebpackSetting.js
+++ src/setUikitWebpackSetting.js
@@ -2,6 +2,16 @@ const setUikitWebpackSetting = (config) => {
   const moduleRulesChanges = {
     rules: [
       ...config.module.rules,
+      // https://github.com/react-native-web-community/react-native-web-webview#getting-started
+      {
+        test: /postMock.html$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
+        },
+      },
       {
         test: /\.(ts|tsx)$/,
         use: [
@@ -25,6 +35,9 @@ const setUikitWebpackSetting = (config) => {
       "react-native": "react-native-web",
       "react-native-linear-gradient": "react-native-web-linear-gradient",
       "react-native-svg": "react-native-svg-web",
+      "react-native-webview": "react-native-web-webview",
+      "react-native-modal": "modal-react-native-web",
+      "react-native-shimmer-placeholder": "react-content-loader",
       "@storybook/react-native": "@storybook/react",
     },
   };
