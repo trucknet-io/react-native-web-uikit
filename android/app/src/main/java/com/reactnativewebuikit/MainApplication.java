8d6eab24760e9b500c17ffb835e4d8a5067658fe
Frolov Sergey <bigcheeseh@gmail.com>
2019-09-05 16:52:51 +0300

    feat(themeProvider): ✨  remove delete react native vector icons changes from this branch [#123123123]

--- android/app/src/main/java/com/reactnativewebuikit/MainApplication.java
+++ android/app/src/main/java/com/reactnativewebuikit/MainApplication.java
@@ -3,7 +3,6 @@ package com.reactnativewebuikit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.horcrux.svg.SvgPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNDeviceInfo(),
             new RNCWebViewPackage(),
             new SvgPackage(),
             new LinearGradientPackage()
